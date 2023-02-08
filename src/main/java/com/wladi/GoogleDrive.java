5d8bc730e3aaf90cc4eef43bdf044b452afb6984
wladi-silva <wladibahia@gmail.com>
2023-02-07 23:20:17 -0300

    metodo de download

--- src/main/java/com/wladi/GoogleDrive.java
+++ src/main/java/com/wladi/GoogleDrive.java
@@ -1,11 +1,10 @@
 package com.wladi;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
@@ -16,23 +15,19 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
 
 public class GoogleDrive {
 
   private static final String APPLICATION_NAME = "ServicoTeste";
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
   private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
+  private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_READONLY);
   private static final String CREDENTIALS_FILE_PATH = "credentials.json";
 
   private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
@@ -60,33 +55,13 @@ public class GoogleDrive {
 
     final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
     Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-        .setApplicationName(APPLICATION_NAME)
-        .build();
+      .setApplicationName(APPLICATION_NAME)
+      .build();
     
-    FileList result = service.files().list()
-        .setPageSize(10)
-        .setFields("nextPageToken, files(id, name)")
-        .execute();
-    List<File> files = result.getFiles();
-    if (files == null || files.isEmpty()) {
-      System.out.println("Não existem dados a serem exibidos.");
-    } else {
-      System.out.println("Arquivos:");
-      for (File file : files) {
-        System.out.printf("%s (%s)\n", file.getName(), file.getId());
-      }
-    }
+    FileOutputStream fileOutputStream = new FileOutputStream("download.pdf");
+    String fileId = "12tLOwj9_AED6SA3CHDzZuEBgJ-K-S7oV";
+    service.files().get(fileId).executeMediaAndDownloadTo(fileOutputStream);
     
-    try {
-      OutputStream outputStream = new ByteArrayOutputStream();
-      service.files().get("1IXz5ny3r7ZVmRouMxnztRztj_NXa2YxV")
-        .executeMediaAndDownloadTo(outputStream);
-    } catch (GoogleJsonResponseException e) {
-      System.err.println("Erro: " + e.getDetails());
-      throw e;
-    }
-    
-
   }
 
 }
